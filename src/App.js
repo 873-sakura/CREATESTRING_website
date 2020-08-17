680c37617985be8a7005c463a3c75a61cf404b11
873-sakura <f5418031@stu.chs.nihon-u.ac.jp>
2020-08-18 00:57:50 +0900

    recommit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,9 @@ import React from "react";
 
 const App = () => {
   return (
-    <Router>
-        <div>
-        <h1>hello world</h1>
-        </div>
-        <footer className="footer">
-            <div className="content has-text-centered">
-            <p>&copy; HelloWorld.pde</p>
-            </div>
-        </footer>
-    </Router>
+    <div>
+    <h1>hello world</h1>
+    </div>
   );
 };
 
